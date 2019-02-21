6e3375af07204acdfe212d06a73c772c91edcfb1
lylloo <tatiana.juinp15@gmail.com>
2019-02-21 19:16:11 +0100

    ajout rapport
    il manque le design

--- application/views/visiteur/ajout/ajoutRapport.php
+++ application/views/visiteur/ajout/ajoutRapport.php
@@ -21,20 +21,28 @@ echo doctype('html5');
 				<?php
 					//METTRE UN ECHO QUI FINI PAR UN POINT VIRGULE ET METTRE . POUR CHAQUE ELLEMENT
 					//REMPLACER LES ; PAR DES . SAUF POUR LE DERNIER QUI EST UN ;
-					form_open("EspaceVisiteur/saisie");
-					form_label("Numero de rapport");
-					form_input("numRapport");
-					br(2);
-					form_label(" Nom du praticient");
+				echo
+					form_open("EspaceVisiteur/saisie").
+					form_label("Numero de rapport ").
+					form_input("numRapport").
+					br(2).
+					form_label(" Nom du praticient ").
 					//REMPACER PAR UN SCROLL , menu qui descent
-					form_input("nomPraticient");
-					br(2);
-					form_label("date");
-					//METTRE "<input type='date' name='date'>"
+					form_input("nomPraticient ").
+					br(2).
+					form_label("date ").
+					"<input type='date' name='date'>".
+					br(2).
 					//FIN DU FORMULAIRE
-					form_label("Motif");
-					form_input("motif");
-
+					form_label("Motif ").
+					form_input("motif").
+					br(2).
+					//description
+					form_label("description").
+					br(1).
+					form_textarea("description").
+					br(2).
+					form_submit("Valider","Ajouter").
 					form_close();
 
 				?>	
