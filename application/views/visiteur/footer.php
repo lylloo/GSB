0e26988cfc4d5510a5461328386955336c5029a9
Loan Besson <loan.besson@hotmail.fr>
2019-02-22 09:18:17 +0100

    Update footer.php

--- application/views/visiteur/footer.php
+++ application/views/visiteur/footer.php
@@ -1,4 +1,4 @@
-            </div>
+            	</div>
 		</div>
 		<p class="footer">Page rendue en <strong>{elapsed_time}</strong> secondes. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 	</div>
