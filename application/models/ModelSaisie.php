83251295ed742bd8dfe64fa5f313f082c1dc3bf4
lylloo <tatiana.juinp15@gmail.com>
2019-02-25 19:02:38 +0100

    modification de ajout
    Debut du tableau quand l'etat est en cour.

--- application/models/ModelSaisie.php
+++ application/models/ModelSaisie.php
@@ -8,8 +8,20 @@ class ModelSaisie extends CI_Model {
             parent::__construct();
     }
     
+    /**
+    * Liste des rapport non valider
+    *@return la liste des rapport non valider 
+    */
     public function rechercheRapport(){
-    	
+    	$query = $this->db->query("SELECT *
+                                    FROM praticien p, rapport_visite r
+                                    WHERE p.PRA_NUM = r.PRA_NUM
+                                    AND ETAT_ID==1
+                                    AND r.VIS_MATRICULE = '".$_SESSION['matricule']."'
+                                    $sql
+                                    
+                                    ");
+    	return $query->result();
     }
 }
 ?>
--- application/views/visiteur/saisie/ajoutRapport.php
+++ application/views/visiteur/saisie/ajoutRapport.php
@@ -1,7 +1,7 @@
 <?php
 	//METTRE UN ECHO QUI FINI PAR UN POINT VIRGULE ET METTRE . POUR CHAQUE ELLEMENT
 	//REMPLACER LES ; PAR DES . SAUF POUR LE DERNIER QUI EST UN ;
-	echo
+	/*echo
 		form_open("EspaceVisiteur/saisie").
 		form_label("Numero de rapport ").
 		form_input("numRapport").
@@ -24,4 +24,35 @@
 		br(2).
 		form_submit("Valider","Ajouter").
 		form_close();
+		*/
 ?>
+<style>
+	.tableau-neutre{
+        margin: auto;
+        width: 850px;
+    }
+
+</style>
+<table class="tableau-neutre">
+	<thead>
+		 <tr>
+            <td colspan="10">
+                <?php 
+                    echo heading('Rapport de visite non valider', '2', 'style=\'text-align:center;\'');
+                ?>
+            </td>
+        </tr>
+
+        <!-- tableau construction -->
+         <tr>
+            <th width="70px">N° rapport</th>
+            <th width="80px">N° praticien</th>
+            <th width="90px">Nom praticien</th>
+            <th width="110px">Prénom praticien</th>
+            <th width="150px">Motif de visite</th>
+            <!--<th width="80px">Date de visite</th>-->
+            <th>Médicaments présentés</th>
+            <th> Modifier le rapport </th>
+        </tr>
+	</thead>
+</table>
